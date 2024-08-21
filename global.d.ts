b25c0b9c8189fdd7f47d7549a6479e3e2516168e
rahulsolanki001 <43613414+rahulsolanki001@users.noreply.github.com>
2024-08-21 15:26:25 +0530

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -4,29 +4,5 @@ export {};
 declare global {
   interface Window {
     fbAsyncInit: () => void;
-    FB: FB; // Specify the type for `FB`
-  }
-}
-
-// Define the FB interface
-interface FB {
-  init(params: { appId: string; cookie: boolean; xfbml: boolean; version: string }): void;
-  AppEvents: {
-    logPageView(): void;
-  };
-  login(callback: (response: FB.StatusResponse) => void, options: { scope: string }): void;
-  api(path: string, options: { fields: string }, callback: (response: any) => void): void;
-}
-
-// Define the StatusResponse interface within the FB namespace
-declare namespace FB {
-  interface StatusResponse {
-    status: string;
-    authResponse: {
-      accessToken: string;
-      expiresIn: number;
-      signedRequest: string;
-      userID: string;
-    };
   }
 }
